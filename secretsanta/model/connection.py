df6bea1a622a68d5c154928147719858304824c2
Dest0re <klacik2002@yandex.ru>
2021-12-20 18:52:02 +0500

    Fix not null check

--- secretsanta/model/connection.py
+++ secretsanta/model/connection.py
@@ -4,13 +4,13 @@ from peewee import MySQLDatabase
 
 # Maybe a class for the constants
 _DATABASE_HOST = os.getenv('DATABASE_HOST')
-_DATABASE_PORT = int(os.getenv('DATABASE_PORT'))
+_DATABASE_PORT = os.getenv('DATABASE_PORT')
 _MYSQL_USERNAME = os.getenv('MYSQL_USERNAME')
 _MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 _DATABASE_NAME = os.getenv('DATABASE_NAME')
 
 try:
-    assert sum(map(bool, (_DATABASE_HOST, _DATABASE_PORT, _MYSQL_USERNAME, _MYSQL_PASSWORD, _DATABASE_NAME)))
+    assert _DATABASE_HOST and _DATABASE_PORT and _MYSQL_USERNAME and _MYSQL_PASSWORD and _DATABASE_NAME
 except AssertionError:
     raise KeyError("Some important stuff is missing.")
 
@@ -18,7 +18,7 @@ except AssertionError:
 connection = MySQLDatabase(
     database=_DATABASE_NAME,
     host=_DATABASE_HOST,
-    port=_DATABASE_PORT,
+    port=int(_DATABASE_PORT),
     user=_MYSQL_USERNAME,
     passwd=_MYSQL_PASSWORD
 )
