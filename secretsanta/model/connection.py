972a3d97b40a990259eb9cd019d32b2bafdb1374
Dest0re <klacik2002@yandex.ru>
2021-12-22 19:25:07 +0500

    Code cleanup

--- secretsanta/model/connection.py
+++ secretsanta/model/connection.py
@@ -1,5 +1,3 @@
-import os
-
 from peewee import MySQLDatabase
 
 from .utils import EnvironmentVariables
@@ -12,19 +10,6 @@ env = EnvironmentVariables(
     'MYSQL_PASSWORD', 
     'DATABASE_NAME'
 )
-
-
-# # Maybe a class for the constants
-# _DATABASE_HOST = os.getenv('DATABASE_HOST')
-# _DATABASE_PORT = os.getenv('DATABASE_PORT')
-# _MYSQL_USERNAME = os.getenv('MYSQL_USERNAME')
-# _MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-# _DATABASE_NAME = os.getenv('DATABASE_NAME')
-
-# try:
-#     assert _DATABASE_HOST and _DATABASE_PORT and _MYSQL_USERNAME and _MYSQL_PASSWORD and _DATABASE_NAME
-# except AssertionError:
-#     raise KeyError("Some important stuff is missing.")
 
 
 connection = MySQLDatabase(
