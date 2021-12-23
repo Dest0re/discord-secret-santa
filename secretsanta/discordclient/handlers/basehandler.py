ce93ad575167207eaa9468299032a10554116333
Dest0re <klacik2002@yandex.ru>
2021-12-24 00:38:33 +0500

    Fix BaseHandler typo

--- secretsanta/discordclient/handlers/basehandler.py
+++ secretsanta/discordclient/handlers/basehandler.py
@@ -21,9 +21,9 @@ class BaseHandler:
             await self._handle(ctx)
         except StopHandleException:
             await self._exc(ctx)
-
-        if self._next:
-            await self._next.do_handle(ctx)
+        else:
+            if self._next:
+                await self._next.do_handle(ctx)
 
     def set_next(self, next_handler):
         self._next = next_handler
