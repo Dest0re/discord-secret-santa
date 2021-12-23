42246214abfa873cd01d7172421f68584fb4589d
Dest0re <klacik2002@yandex.ru>
2021-12-23 18:28:38 +0500

    Add TitledText and DebugText classes

--- secretsanta/utils/embed.py
+++ secretsanta/utils/embed.py
@@ -8,3 +8,13 @@ class EmbedText(discord.Embed):
 class ErrorText(EmbedText):
     def __init__(self, text: str):
         super().__init__(text, color=0xFF0000)
+
+
+class TitledText(discord.Embed):
+    def __init__(self, title: str, text: str, color=0xFFFFFF):
+        super().__init__(title=title, description=text, color=color)
+
+
+class DebugText(TitledText):
+    def __init__(self, text: str):
+        super().__init__('Это дебаг, он тут ненадолго', text, color=0xFF00FF)
