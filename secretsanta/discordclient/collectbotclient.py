82e1e1300d6e74821e742ace8f3b5f8fcabcf647
Dest0re <klacik2002@yandex.ru>
2021-12-23 17:26:34 +0500

    Rename BotClient to CollectBotClient

--- secretsanta/discordclient/collectbotclient.py
+++ secretsanta/discordclient/collectbotclient.py
@@ -1,17 +1,14 @@
-import asyncio
-
-import discord
 from discord.ext import commands
 
 from .debugcog import DebugCog
 from .presentcog import PresentCog
 
 
-class BotClient(commands.Bot):
+class CollectBotClient(commands.Bot):
     async def on_ready(self):
         print(f'Logged in Discord as {self.user}')
 
 
-bot = BotClient(debug_guild=920707642308055100)
+bot = CollectBotClient(debug_guild=920707642308055100)
 bot.add_cog(DebugCog(bot))
 bot.add_cog(PresentCog(bot))
