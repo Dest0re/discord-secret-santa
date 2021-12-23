486336c0ef3c472c71c6212d952b84b6fbefc18f
Dest0re <klacik2002@yandex.ru>
2021-12-23 23:50:08 +0500

    Add name field to database scheme and update the model

--- secretsanta/model/gamepackage.py
+++ secretsanta/model/gamepackage.py
@@ -1,4 +1,4 @@
-from peewee import PrimaryKeyField, ForeignKeyField, BigIntegerField, FloatField
+from peewee import PrimaryKeyField, ForeignKeyField, BigIntegerField, FloatField, TextField
 
 from .basemodel import BaseModel
 from .gamerequirements import GameRequirements
@@ -9,6 +9,7 @@ class GamePackage(BaseModel):
     requirements = ForeignKeyField(GameRequirements, column_name='game_requirements_id')
     steam_id = BigIntegerField(column_name='game_package_steam_id')
     price = FloatField(column_name='price')
+    name = TextField(column_name='game_package_name')
 
     class Meta:
         table_name = 'GamePackage'
