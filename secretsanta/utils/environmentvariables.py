c758bc4491ae50ea6945915da5a9044ee730a66b
Dest0re <klacik2002@yandex.ru>
2021-12-24 03:26:20 +0500

    Make EnvironmentVariable immutable

--- secretsanta/utils/environmentvariables.py
+++ secretsanta/utils/environmentvariables.py
@@ -1,17 +1,27 @@
 import os
 
-# TODO: Make immutable
+
 class EnvironmentVariables:
     def __init__(self, *args: str):
-        
         for name in args:
             var = os.getenv(name)
 
             if var == None:
                 raise KeyError(f'Missing important variable named {name}')
 
-            setattr(self, name, var)
+            object.__setattr__(self, name, var)
+    
+
+    def __setattr__(self, *args) -> None:
+        raise TypeError(__class__.__name__ + ' is immutable')
+
+    def __delattr__(self, *args) -> None:
+        raise TypeError(__class__.__name__ + ' is immutable')
             
 
 if __name__ == '__main__':
-    print(EnvironmentVariables('DATABASE_PORT').DATABASE_PORT)
+    var = EnvironmentVariables('DATABASE_PORT')
+    print(var.DATABASE_PORT)
+    var.DATABASE_PORT = 123
+    print(var.DATABASE_PORT)
+
