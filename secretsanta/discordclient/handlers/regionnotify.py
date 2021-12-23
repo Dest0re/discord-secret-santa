895e17527720b905762526b58eba0601a880ef8a
Dest0re <klacik2002@yandex.ru>
2021-12-23 17:20:01 +0500

    Integrate text_strings feature

--- secretsanta/discordclient/handlers/regionnotify.py
+++ secretsanta/discordclient/handlers/regionnotify.py
@@ -5,6 +5,8 @@ from discord.enums import ButtonStyle
 
 from .basehandler import BaseHandler
 from utils.embed import EmbedText
+from utils.strings import text_strings as ts
+
 
 class AcceptButton(discord.ui.Button):
         def __init__(self, member: discord.Member):
@@ -19,7 +21,7 @@ class AcceptButton(discord.ui.Button):
         async def callback(self, interaction: discord.Interaction):
             if self._is_active:
                 if interaction.user != self._member:
-                    await interaction.response.send_message(embed=EmbedText('Это не твоё.'), ephemeral=True)
+                    await interaction.response.send_message(embed=EmbedText(ts.someone_elses_interaction_warning), ephemeral=True)
                 else:
                     self._is_active = False
                     self.disabled = True
@@ -34,7 +36,7 @@ class RegionsNotify(BaseHandler):
         accept_button = AcceptButton(ctx.author)
         view = discord.ui.View(accept_button)
         await ctx.respond(
-            embed=EmbedText("Сейчас мы поддерживаем не все регионы. Если вы живёте не в одном из этих: (список), то будьте осторожны: вы всё ещё сможете подарить игру по вашей региональной цене, но вряд ли мы сумеем отправить подарок вам в ответ!"),
+            embed=EmbedText(ts.region_warning),
             view=view
         )
 
