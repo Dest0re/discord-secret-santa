d4e7eb583e99e822acc2b3d47a7790dfdbf99bff
Dest0re <klacik2002@yandex.ru>
2021-12-23 18:30:37 +0500

    Add register_simple_user command

--- secretsanta/discordclient/debugcog.py
+++ secretsanta/discordclient/debugcog.py
@@ -3,11 +3,18 @@ from discord.commands import SlashCommandGroup
 from discord.ext import commands
 
 from .basecog import BaseCog
+from .handlers import SaveBasicUserInformation, TestHandler
 
 
 class DebugCog(BaseCog):
     debug_group = SlashCommandGroup('debug', 'Debug features. Admin only!', guild_ids=[920707642308055100])
 
     @debug_group.command(name='ping')
-    async def _ping_command(self, ctx: discord.commands.ApplicationContext):
+    async def _ping_command(self, ctx: discord.ApplicationContext):
         await ctx.respond('Pong!')
+
+    @debug_group.command(name='register_simple_user', guild_ids=[920707642308055100])
+    async def _register_simple_user(self, ctx: discord.ApplicationContext):
+        handler = SaveBasicUserInformation()
+        handler.set_next(TestHandler())
+        await handler.do_handle(ctx)
