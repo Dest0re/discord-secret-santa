d1d7d37e304f9b73e63fbe7a5c84a1edb2ec7754
Dest0re <klacik2002@yandex.ru>
2021-12-24 22:42:05 +0500

    Add game package select

--- secretsanta/discordclient/handlers/selectgame.py
+++ secretsanta/discordclient/handlers/selectgame.py
@@ -5,12 +5,55 @@ import discord
 
 from .basehandler import BaseHandler, StopHandleException
 from utils.strings import text_strings as ts
-from utils.embed import EmbedText, ErrorText, DebugText
+from utils.embed import EmbedText, ErrorText, DebugText, TitledText, WarningText, SuccessText
+from steammarket import SteamStore, Game
+from steammarket.exceptions import AppDoesNotExist
+from .view.dropdown import GamePackageChooseDropdown, PackageSelectOption
 
-game_url_regex = r'[a-z]+://store.steampowered.com/app/\d+/[a-zA-Z]+/?'
+game_url_regex = r'[a-z]+://store.steampowered.com/app/(?P<game_id>\d+)/[a-zA-Z]+/?'
+
+steam_store = SteamStore()
+
+class InvalidGameUrl(Exception):
+    pass
 
 
 class SelectGameHandler(BaseHandler):
+    async def _get_game_from_url(self, game_url):
+        match = re.match(game_url_regex, game_url)
+
+        if not match:
+            raise InvalidGameUrl
+
+        game_id = match.group('game_id')
+
+        game = await steam_store.fetch_app(int(game_id))
+
+        return game
+
+    async def _get_one_package(self, ctx: discord.ApplicationContext, game: Game):
+        dropdown = GamePackageChooseDropdown(
+            user=ctx.author,
+            options=map(lambda gp: PackageSelectOption(gp), game.packages)
+        )
+
+        message = await ctx.respond(embed=EmbedText(ts.multiple_packages), view=discord.ui.View(dropdown))
+
+        try:
+            package = (await dropdown.get_choices(120)).game_package
+        except asyncio.TimeoutError:
+            await ctx.respond(embed=ErrorText(ts.timeout_error))
+            raise StopHandleException
+
+        dropdown.disabled = True
+        await message.edit(
+            embed=SuccessText(ts.selected_game.format(game_package_name=package.name)), 
+            view=discord.ui.View(dropdown)
+        )
+
+        return package
+
+
     async def _handle(self, ctx: discord.ApplicationContext):
         await ctx.respond(embed=EmbedText(ts.game_url_request))
 
@@ -23,11 +66,27 @@ class SelectGameHandler(BaseHandler):
                     timeout=300
                 )
 
-                if re.match(game_url_regex, message.content):
-                    await ctx.respond(embed=DebugText("Тут должен быть выбор пакета через выпадающий список"))
-                    break
+                try:
+                    game = await self._get_game_from_url(message.content)
+
+                    game_package = None
+
+                    if len(game.packages) > 1:
+                        game_package = await self._get_one_package(ctx, game)
+                    else:
+                        game_package = game.packages[0]
+                    
+                    await ctx.respond(embed=DebugText(f"Выбрана игра: {game_package.name}"))
+
+                except InvalidGameUrl:
+                    await ctx.respond(embed=WarningText(ts.invalid_game_url))
+                    continue
+                except AppDoesNotExist:
+                    await ctx.respond(embed=WarningText(ts.app_does_not_exist))
+                    continue
                 else:
-                    await ctx.respond(embed=ErrorText(ts.invalid_game_url))
+                    break
+                    
             else:
                 await ctx.respond(embed=ErrorText(ts.url_get_error))
                 raise StopHandleException("Game url get")
