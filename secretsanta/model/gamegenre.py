03210ce1f6b5e82d4a7e85989747be4fb4368704
Dest0re <klacik2002@yandex.ru>
2021-12-27 00:26:10 +0500

    Fix GameGenre model

--- secretsanta/model/gamegenre.py
+++ secretsanta/model/gamegenre.py
@@ -5,7 +5,7 @@ from .basemodel import BaseModel
 
 class GameGenre(BaseModel):
     id = PrimaryKeyField(column_name='game_genre_id')
-    genre_name = TextField(column_name='genre_name')
+    name = TextField(column_name='game_genre_name')
 
     class Meta:
         table_name = 'GameGenre'
