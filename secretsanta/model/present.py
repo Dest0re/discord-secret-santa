d5f86dc6bc3cbcd13e4a1ef3c24ec0d0f68172ba
Dest0re <klacik2002@yandex.ru>
2021-12-26 10:38:22 +0500

    Change database schema

--- secretsanta/model/present.py
+++ secretsanta/model/present.py
@@ -1,4 +1,4 @@
-from peewee import PrimaryKeyField, ForeignKeyField
+from peewee import PrimaryKeyField, ForeignKeyField, BooleanField
 
 from .basemodel import BaseModel
 from .user import User
@@ -9,6 +9,7 @@ class Present(BaseModel):
     id = PrimaryKeyField(column_name='present_id')
     user = ForeignKeyField(User, column_name='user_id')
     game_package = ForeignKeyField(GamePackage, column_name='game_package_id')
+    paid = BooleanField(column_name='is_paid')
 
     class Meta:
         table_name = 'Present'
