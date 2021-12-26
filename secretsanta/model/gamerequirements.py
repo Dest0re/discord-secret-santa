b43b69aec623a685d7a9d8d96324e10fab458e09
Dest0re <klacik2002@yandex.ru>
2021-12-26 10:37:32 +0500

    Update database schema

--- secretsanta/model/gamerequirements.py
+++ secretsanta/model/gamerequirements.py
@@ -1,4 +1,4 @@
-from peewee import PrimaryKeyField, TextField, ForeignKeyField
+from peewee import PrimaryKeyField, TextField, ForeignKeyField, BigIntegerField
 
 from .basemodel import BaseModel
 from .performance import Performance
@@ -7,7 +7,9 @@ from .performance import Performance
 class GameRequirements(BaseModel):
     id = PrimaryKeyField(column_name='game_requirements_id')
     performance = ForeignKeyField(Performance, column_name='performance_id')
-    name = TextField(column_name='game_requirements_name')
+    name = TextField(column_name='requirements_name')
+    description = TextField(column_name='requirements_description')
+    emoji_id = BigIntegerField(column_name='emoji_id')
 
     class Meta:
         table_name = 'GameRequirements'
--- secretsanta/model/pcperformance.py
+++ secretsanta/model/pcperformance.py
@@ -1,4 +1,4 @@
-from peewee import PrimaryKeyField, TextField, ForeignKeyField
+from peewee import PrimaryKeyField, TextField, ForeignKeyField, BigIntegerField
 
 from .basemodel import BaseModel
 from .performance import Performance
@@ -8,6 +8,8 @@ class PCPerformance(BaseModel):
     id = PrimaryKeyField(column_name='pc_performance_id')
     performance = ForeignKeyField(Performance, column_name='performance_id')
     name = TextField(column_name='pc_performance_name')
+    description = TextField(column_name='pc_performance_description')
+    emoji_id = BigIntegerField(column_name='emoji_id')
 
     class Meta:
         table_name = 'PCPerformance'
