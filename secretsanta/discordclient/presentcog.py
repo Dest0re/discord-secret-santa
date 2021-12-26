dc9b9ee4df5d5a2b69623abc594df97346fe6fd5
Dest0re <klacik2002@yandex.ru>
2021-12-26 10:39:04 +0500

    Construct the full event chain

--- secretsanta/discordclient/presentcog.py
+++ secretsanta/discordclient/presentcog.py
@@ -2,7 +2,10 @@ import discord
 from discord.commands import slash_command
 
 from .basecog import BaseCog
-from .handlers import RegionsNotify, SaveBasicUserInformation, TestHandler
+from .handlers import RegionsNotify, SaveBasicUserInformation, \
+    SelectGameHandler, PaymentHandler, SelectGameGenresHandler, \
+    SelectGameRequirementsHandler, SelectPreferredGenresHandler, \
+    SelectPCPerformanceHandler, SuccessHandler
 
 from typing import Optional
 
@@ -11,5 +14,15 @@ class PresentCog(BaseCog):
     @slash_command(name='present', guild_ids=[920707642308055100])
     async def _present_command(self, ctx: discord.ApplicationContext, game_url: Optional[str] = None):
         handler = RegionsNotify()
-        handler.set_next(SaveBasicUserInformation()).set_next(TestHandler())
+        (
+            handler 
+            .set_next(SaveBasicUserInformation())
+            .set_next(SelectGameHandler()) 
+            .set_next(PaymentHandler()) 
+            .set_next(SelectGameGenresHandler()) 
+            .set_next(SelectGameRequirementsHandler()) 
+            .set_next(SelectPreferredGenresHandler()) 
+            .set_next(SelectPCPerformanceHandler()) 
+            .set_next(SuccessHandler())
+        )
         await handler.do_handle(ctx)
