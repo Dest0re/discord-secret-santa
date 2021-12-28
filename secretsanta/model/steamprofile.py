e1639efbedf17b1a6ce925e1c56afdabd378abf3
Dest0re <klacik2002@yandex.ru>
2021-12-28 15:06:13 +0500

    Change database schema

--- secretsanta/model/steamprofile.py
+++ secretsanta/model/steamprofile.py
@@ -5,7 +5,8 @@ from .basemodel import BaseModel
 
 class SteamProfile(BaseModel):
     id = PrimaryKeyField(column_name='steam_profile_id')
-    steam_id = BigIntegerField(column_name='steam_id')
+    steam_id32 = BigIntegerField(column_name='steam_id32')
+    steam_id64 = BigIntegerField(column_name='steam_id64')
 
     class Meta:
         table_name = 'SteamProfile'
