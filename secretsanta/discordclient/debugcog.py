b8f3b71d330d39802a0e6412a7769d9787b280ba
Dest0re <klacik2002@yandex.ru>
2021-12-28 15:09:21 +0500

    Add AskForSteamUrlHandler to debug cog

--- secretsanta/discordclient/debugcog.py
+++ secretsanta/discordclient/debugcog.py
@@ -3,7 +3,7 @@ from discord.commands import SlashCommandGroup
 from discord.ext import commands
 
 from .basecog import BaseCog
-from .handlers import SaveBasicUserInformation, TestHandler, SelectGameHandler, SelectGameRequirementsHandler, SelectGameGenresHandler
+from .handlers import SaveBasicUserInformation, TestHandler, SelectGameHandler, SelectGameRequirementsHandler, SelectGameGenresHandler, SelectPCPerformanceHandler, AskForSteamUrlHandler
 
 
 class DebugCog(BaseCog):
@@ -26,13 +26,25 @@ class DebugCog(BaseCog):
         await handler.do_handle(ctx)
 
     @debug_group.command(name='select_game_requirements', guild_ids=[920707642308055100])
-    async def _register_new_game(self, ctx: discord.ApplicationContext):
+    async def _select_game_requirements(self, ctx: discord.ApplicationContext):
         handler = SelectGameRequirementsHandler()
+        handler.set_next(TestHandler())
+        await handler.do_handle(ctx)
+
+    @debug_group.command(name='select_pc_performance', guild_ids=[920707642308055100])
+    async def _select_pc_performance(self, ctx: discord.ApplicationContext):
+        handler = SelectPCPerformanceHandler()
         handler.set_next(TestHandler())
         await handler.do_handle(ctx)
 
     @debug_group.command(name='select_game_genres', guild_ids=[920707642308055100])
     async def _select_game_genres(self, ctx: discord.ApplicationContext):
         handler = SelectGameGenresHandler()
+        handler.set_next(TestHandler())
+        await handler.do_handle(ctx)
+
+    @debug_group.command(name='add_steam_profile', guild_ids=[920707642308055100])
+    async def _add_steam_profile(self, ctx: discord.ApplicationContext):
+        handler = AskForSteamUrlHandler()
         handler.set_next(TestHandler())
         await handler.do_handle(ctx)
