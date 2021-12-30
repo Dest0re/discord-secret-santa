19db7c879d951e2a2641f13e37b226debcff626d
Dest0re <klacik2002@yandex.ru>
2021-12-30 19:55:38 +0500

    Integrate new handlers

--- secretsanta/discordclient/giftcog.py
+++ secretsanta/discordclient/giftcog.py
@@ -5,24 +5,29 @@ from .basecog import BaseCog
 from .handlers import RegionsNotify, SaveBasicUserInformation, \
     SelectGameHandler, PaymentHandler, SelectGameGenresHandler, \
     SelectGameRequirementsHandler, SelectPreferredGenresHandler, \
-    SelectPCPerformanceHandler, SuccessHandler, AskForSteamUrlHandler, MinimalPriceNotify
+    SelectPCPerformanceHandler, SuccessHandler, AskForSteamUrlHandler, \
+    MinimalPriceNotify, CheckIfInProcess, BeginGiftingProcess, EndGiftingProcess, AddGiftMessage
 
 
 class GiftCog(BaseCog):
-    @slash_command(name='gift', guild_ids=[920707642308055100])
+    @slash_command(name='gift', guild_ids=[920707642308055100, 625032816576561170])
     async def _present_command(self, ctx: discord.ApplicationContext):
-        handler = RegionsNotify()
+        handler = SaveBasicUserInformation()
         (
-            handler 
-            .set_next(SaveBasicUserInformation())
+            handler
+            .set_next(CheckIfInProcess())
+            .set_next(BeginGiftingProcess())
+            .set_next(RegionsNotify())
             .set_next(AskForSteamUrlHandler())
             .set_next(MinimalPriceNotify())
             .set_next(SelectGameHandler()) 
             .set_next(PaymentHandler()) 
+            .set_next(AddGiftMessage())
             .set_next(SelectGameGenresHandler()) 
             .set_next(SelectGameRequirementsHandler()) 
             .set_next(SelectPreferredGenresHandler()) 
             .set_next(SelectPCPerformanceHandler()) 
             .set_next(SuccessHandler())
+            .set_next(EndGiftingProcess())
         )
         await handler.do_handle(ctx)
