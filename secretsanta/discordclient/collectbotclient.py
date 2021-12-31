71619139069b02773f59247455134945fa27953f
Dest0re <klacik2002@yandex.ru>
2021-12-31 16:37:18 +0500

    Add command exceptions handling

--- secretsanta/discordclient/collectbotclient.py
+++ secretsanta/discordclient/collectbotclient.py
@@ -1,17 +1,26 @@
 import discord
+from discord.commands import errors
 from discord.ext import commands
+from discord.ext.commands.errors import PrivateMessageOnly
 
 from .debugcog import DebugCog
 from .giftcog import GiftCog
 from model import User
+from utils.embed import ErrorText
+
+from utils.strings import text_strings as ts
 
 
 class CollectBotClient(commands.Bot):
     async def on_ready(self):
         User.update(in_process=False).execute()
 
-        await self.change_presence(activity=discord.Game('/gift в ЛС!'))
+        await self.change_presence(activity=discord.Game(ts.collection_stage_status_text))
         print(f'Logged in Discord as {self.user}')
+
+    async def on_command_error(self, ctx: discord.ApplicationContext, error):
+        if isinstance(error, commands.errors.PrivateMessageOnly):
+            await ctx.respond(embed=ErrorText(ts.only_dm_error), ephemeral=True)
 
 
 bot = CollectBotClient()
