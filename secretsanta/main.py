9f3a0887b224d5664dfe58ad6154565b63df8177
Dest0re <klacik2002@yandex.ru>
2022-01-05 21:56:44 +0500

    Update main.py

--- secretsanta/main.py
+++ secretsanta/main.py
@@ -1,9 +1,29 @@
+from enum import Enum
 from utils import EnvironmentVariables
-from discordclient import bot
 from model import PCPerformance
+
+
+class WorkModes(Enum):
+    collecting=1,
+    preparing=2,
+    back_gifting=3
+
+
+WORK_MODE = WorkModes.back_gifting
+
+if WORK_MODE == WorkModes.collecting:
+    from discordclient import bot
+elif WORK_MODE == WorkModes.preparing:
+    from discordclient.friendrequests import SendFriendRequestsBotClient
+    bot = SendFriendRequestsBotClient()
+elif WORK_MODE == WorkModes.back_gifting:
+    from discordclient.giftingconcept import PresentBot
+    bot = PresentBot()
+
 
 bot_env = EnvironmentVariables('DISCORD_BOT_TOKEN')
 
 
 if __name__ == '__main__':
     bot.run(bot_env.DISCORD_BOT_TOKEN)
+    pass
